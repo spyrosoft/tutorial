1e6be8da0b0ae866d9967c372f48e8359006e290
Bennett Roesch <meta@spyrosoft.com>
2016-01-12 14:51:39 -0800

    Convert sections from an array to an object.
    Sections need to be looked up via an identifier.

--- js/tutorial.js
+++ js/tutorial.js
@@ -1,56 +1,61 @@
 var Tutorial = function() {
-	var sections = new Array();
+	var sections = new Object();
 	
 	return {
-		addSection : function( new_title, new_intro ) {
-			var title = "";
-			var intro = "";
-			var correct_problems = "";
-			var incorrect_problems = "";
+		addSection : function( new_section_identifier, new_title, new_intro ) {
+			var identifier = new_section_identifier;
+			var title = new_title;
+			var intro = new_intro;
+			var correct_problems = new Array();
+			var incorrect_problems = new Array();
 			var problems = new Array();
 			
 			//TODO: Try breaking out the bigger functions into functions to avoid clutter
-			sections.push(
-				{
-					"title" : function() {
-						return title;
-					},
-					"intro" : function() {
-						return intro;
-					},
-					"correctProblems" : function() {
-						return correct_problems;
-					},
-					"incorrectProblems" : function() {
-						return incorrect_problems;
-					},
-					"addProblem" : function( problem_identifier, answer_or_answers ) {
-						if ( typeof answer_or_answers === "string"
-							 || answer_or_answers instanceof Array ) {
-								 problems[ problem_identifier ] = answer_or_answers;
-							 } else {
-								 throw "The new problem is not of type string or Array.";
-							 }
-					},
-					"checkProblem" : function( problem_identifier, answer ) {
-						var current_problem = problems[ problem_identifier ];
-						if ( typeof current_problem === "string" ) {
-							return current_problem === answer;
-						} else if ( current_problem instanceof Array ) {
-							var problem_correct = false;
-							current_problem.forEach(
-								function( current_problem_answer ) {
-									if ( current_problem_answer === answer ) {
-										problem_correct = true;
-									}
-								}
-							);
-							return problem_correct;
-						}
-						return false;
+			sections[ identifier ] = {
+				"title" : function() {
+					return title;
+				},
+				"intro" : function() {
+					return intro;
+				},
+				"correctProblems" : function() {
+					return correct_problems;
+				},
+				"incorrectProblems" : function() {
+					return incorrect_problems;
+				},
+				"addProblem" : function( problem_identifier, answer_or_answers ) {
+					if ( typeof problem_identifier !== "string" ) {
+						throw "The new problem identifier is not a string.";
 					}
+					if ( typeof answer_or_answers === "string"
+					|| answer_or_answers instanceof Array ) {
+						problems[ problem_identifier ] = answer_or_answers;
+					} else {
+						throw "The new problem is not a string or Array.";
+					}
+				},
+				"checkProblem" : function( problem_identifier, answer ) {
+					var current_problem = problems[ problem_identifier ];
+					if ( typeof current_problem === "string" ) {
+						return current_problem === answer;
+					} else if ( current_problem instanceof Array ) {
+						var problem_correct = false;
+						current_problem.forEach(
+							function( current_problem_answer ) {
+								if ( current_problem_answer === answer ) {
+									problem_correct = true;
+								}
+							}
+						);
+						return problem_correct;
+					}
+					return false;
 				}
-			);
+			};
+		},
+		getSection : function( section_identifier ) {
+			return sections[ section_identifier ];
 		}
 	};
 };
