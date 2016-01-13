fe0998fb5fa436257bd8e4dd39c17a173c0e9978
Bennett Roesch <meta@spyrosoft.com>
2016-01-12 16:48:29 -0800

    Separate Sections and Problems into closures.

--- js/tutorial.js
+++ js/tutorial.js
@@ -1,61 +1,87 @@
 var Tutorial = function() {
 	var sections = new Object();
 	
-	return {
-		addSection : function( new_section_identifier, new_title, new_intro ) {
-			var identifier = new_section_identifier;
-			var title = new_title;
-			var intro = new_intro;
-			var correct_problems = new Array();
-			var incorrect_problems = new Array();
-			var problems = new Array();
-			
-			//TODO: Try breaking out the bigger functions into functions to avoid clutter
-			sections[ identifier ] = {
-				"title" : function() {
-					return title;
-				},
-				"intro" : function() {
-					return intro;
-				},
-				"correctProblems" : function() {
-					return correct_problems;
-				},
-				"incorrectProblems" : function() {
-					return incorrect_problems;
-				},
-				"addProblem" : function( problem_identifier, answer_or_answers ) {
-					if ( typeof problem_identifier !== "string" ) {
-						throw "The new problem identifier is not a string.";
-					}
-					if ( typeof answer_or_answers === "string"
-					|| answer_or_answers instanceof Array ) {
-						problems[ problem_identifier ] = answer_or_answers;
-					} else {
-						throw "The new problem is not a string or Array.";
-					}
-				},
-				"checkProblem" : function( problem_identifier, answer ) {
-					var current_problem = problems[ problem_identifier ];
-					if ( typeof current_problem === "string" ) {
-						return current_problem === answer;
-					} else if ( current_problem instanceof Array ) {
-						var problem_correct = false;
-						current_problem.forEach(
-							function( current_problem_answer ) {
-								if ( current_problem_answer === answer ) {
-									problem_correct = true;
-								}
-							}
-						);
-						return problem_correct;
-					}
-					return false;
+	var Section = function( new_title, new_intro ) {
+		var title = new_title;
+		var intro = new_intro;
+		var correct_problems = new Array();
+		var incorrect_problems = new Array();
+		var problems = new Array();
+		
+		//TODO: Try breaking out the bigger functions into functions to avoid clutter
+		return {
+			'title' : function() {
+				return title;
+			},
+			'intro' : function() {
+				return intro;
+			},
+			'correctProblems' : function() {
+				return correct_problems;
+			},
+			'incorrectProblems' : function() {
+				return incorrect_problems;
+			},
+			'addProblem' : function( identifier, prompt, answer_or_answers, explanation ) {
+				var newProblem = new Problem( identifier, prompt, answer_or_answers, explanation );
+				problems[ identifier ] = newProblem;
+			},
+			'addProblemAnswer' : function( identifier, new_answer ) {
+				if ( problems[ identifier ] instanceof Array ) {
+					problems[ identifier ].push( new_answer );
+				} else {
+					var current_answer = problems[ identifier ];
+					problems[ identifier ] = new Array();
+					problems[ identifier ].push( current_answer );
+					problems[ identifier ].push( new_answer );
 				}
-			};
+			},
+			'checkProblem' : function( identifier, answer ) {
+				var current_problem = problems[ identifier ];
+				if ( current_problem instanceof Array ) {
+					var problem_correct = false;
+					current_problem.forEach(
+						function( current_problem_answer ) {
+							if ( current_problem_answer === answer ) {
+								problem_correct = true;
+							}
+						}
+					);
+					return problem_correct;
+				} else {
+					return current_problem === answer;
+				}
+				return false;
+			}
+		};
+	};
+	
+	var Problem = function( new_prompt, new_answer_or_answers, new_explanation ) {
+		var prompt = new_prompt;
+		var answer_or_answers = new_answer_or_answers;
+		var explanation = new_explanation;
+		
+		return {
+			'prompt' : function() {
+				return prompt;
+			},
+			'answerOrAnswers' : function() {
+				return answer_or_answers;
+			},
+			'newExplanation' : function() {
+				return explanation;
+			}
+		};
+	};
+	
+	return {
+		addSection : function( identifier, title, intro ) {
+			var newSection = new Section( title, intro );
+			sections[ identifier ] = newSection;
+			return newSection;
 		},
-		getSection : function( section_identifier ) {
-			return sections[ section_identifier ];
+		getSection : function( identifier ) {
+			return sections[ identifier ];
 		}
 	};
 };
