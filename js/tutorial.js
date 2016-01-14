41a03c616a230e8a967beb83925cd90a36ad902d
Bennett Roesch <meta@spyrosoft.com>
2016-01-14 15:04:12 -0800

    Overhaul; keep track of currentProblemSet, problemSet lookup, etc.

--- js/tutorial.js
+++ js/tutorial.js
@@ -5,70 +5,217 @@ var Tutorial = function() {
 		var title = newTitle;
 		var intro = newIntro;
 		var retries = 3;
+		
+		// Full problems which can be looked up by identifier
 		var problems = new Object();
-		var correctProblems = new Array();
-		var incorrectProblems = new Array();
-		var remainingProblems = new Array();
+		
+		// Problem identifiers only (no use having redundant data) which can be looked up by index
+		var problemSets = {
+			'remaining' : new Array(),
+			'correct' : new Array(),
+			'incorrect' : new Array(),
+			'retries' : new Array()
+		};
+		
+		// Problem identifier of the current problem and the previous problem
+		var currentProblem;
+		var previousProblem;
+		
+		// Needed when problemType is 'random'; the value may either be remaining or incorrect in this case
+		var currentProblemSet;
+		
+		var problemType = 'random';
+		
+		// Used to locate and set the next problem for a given problemType
+		var nextProblemLookupByType = {
+			'random' : function() { nextProblemRandom(); },
+			'remaining' : function() { nextProblemByType( 'remaining' ); },
+			'incorrect' : function() { nextProblemByType( 'incorrect' ); },
+			'correct' : function() { nextProblemByType( 'correct' ); }
+		};
+		
+		// All types but random; the others are straight forward
+		var nextProblemByType = function( problemSet ) {
+			setRandomCurrentProblemFromSet( problemSet );
+			currentProblemSet = problemSet;
+		};
+		
+		var nextProblemRandom = function() {
+			var problemSet = remainingOrIncorrectProblemSet();
+			if ( problemSet === null ) { setCurrentProblem( null ); return; }
+			var problemIndex = getRandomProblemIndex( problemSet );
+			problemSet.splice( problemIndex, 1 );
+			var problemIdentifier = problemSet[ problemIdentifier ];
+			setCurrentProblem( problemIdentifier );
+		};
+		
+		// For the random problemType, choose appropriately between the remaining or incorrect problem sets
+		var remainingOrIncorrectProblemSet = function() {
+			if ( problemSets[ 'remaining' ].length === 0 && problemSets[ 'incorrect' ].length === 0 ) { return null; }
+			else if ( problemSets[ 'remaining' ].length === 0 ) { return 'incorrect'; }
+			else if ( problemSets[ 'incorrect' ].length === 0 ) { return 'remaining'; }
+			
+			var totalAvailableProblems = problemSets[ 'remaining' ].length + problemSets[ 'incorrect' ].length;
+			var randomIndex = Math.floor( Math.random() * totalAvailableProblems );
+			//TODO: Is this an off by one error? It pretty much doesn't matter here, just curious.
+			var problemSet = 'remaining';
+			if ( randomIndex < problemSets[ 'incorrect' ].length ) {
+				problemSet = 'incorrect';
+			}
+			return problemSet;
+		};
+		
+		var setRandomCurrentProblemFromSet = function( problemSet ) {
+			var problemIndex = getRandomProblemIndex( problemSet );
+			setCurrentProblem( problemSet[ problemIndex ] );
+		};
+		
+		var nextProblemRemaining = function() {
+			
+		};
+		
+		var nextProblemIncorrect = function() {
+			
+		};
+		
+		var nextProblemCorrect = function() {
+			
+		};
+		
+		var getRandomProblemIndex = function( problemSet ) {
+			if ( problemSet.length === 0 ) { return null; }
+			if ( ! uniqueProblemsExistInSet( problemSet ) ) {
+				return 0;
+			}
+			var problemIndex = Math.floor( Math.random() * problemSet.length );
+			if ( problemSet[ problemIndex ] === previousProblem ) {
+				return getRandomProblemIndex( problemSet );
+			}
+			return problemIndex;
+		};
+		
+		var uniqueProblemsExistInSet = function( problemSet ) {
+			var uniqueProblemExists = false;
+			var firstProblem = problemSet[ 0 ];
+			var secondProblem;
+			for ( var i = 1; i < problemSet.length; i++ ) {
+				secondProblem = problemSet[ i ];
+				if ( secondProblem != firstProblem ) {
+					uniqueProblemExists = true;
+					break;
+				}
+				firstProblem = secondProblem;
+			}
+			return uniqueProblemExists;
+		};
+		
+		var getProblemByIndex = function( problemSet, problemIndex ) {
+			var problemIdentifier = problemSet[ problemIndex ];
+			var newProblem = problems[ problemIdentifier ];
+			newProblem[ 'identifier' ] = problemIdentifier;
+			return newProblem;
+		};
+		
+		var setCurrentProblem = function( identifier, problemSet ) {
+			previousProblem = currentProblem;
+			currentProblem = identifier;
+		};
+		
 		
 		//TODO: Try moving the bigger functions elsewhere to avoid clutter
 		return {
-			'title' : function() {
-				return title;
-			},
-			'intro' : function() {
-				return intro;
-			},
-			'remainingProblems' : function() {
-				return remainingProblems;
-			},
-			'correctProblems' : function() {
-				return correctProblems;
-			},
-			'incorrectProblems' : function() {
-				return incorrectProblems;
-			},
+			// Access the read-only closed variables
+			'title' : function() { return title; },
+			'intro' : function() { return intro; },
+			'problems' : function () { return problems; },
+			'problemsRemaining' : function() { return problemSets[ 'remaining' ]; },
+			'problemsCorrect' : function() { return problemSets[ 'correct' ]; },
+			'problemsIncorrect' : function() { return problemSets[ 'incorrect' ]; },
+			'problemsRetries' : function() { return problemSets[ 'retries' ]; },
+			
+			// This may cause issues if the user wants to use an array as the value for a single answer - it will be treated as multiple answers
 			'addProblem' : function( identifier, prompt, answerOrAnswers, explanation ) {
 				var newProblem = new Problem( prompt, answerOrAnswers, explanation );
 				problems[ identifier ] = newProblem;
-				remainingProblems.push( identifier );
+				problemSets[ 'remaining' ].push( identifier );
 			},
+			
 			'addProblemAnswer' : function( identifier, newAnswer ) {
-				if ( remainingProblems[ identifier ] instanceof Array ) {
-					remainingProblems[ identifier ].push( newAnswer );
+				var problemsRemaining = problemSets[ 'remaining' ];
+				if ( problemsRemaining[ identifier ] instanceof Array ) {
+					problemsRemaining[ identifier ].push( newAnswer );
 				} else {
-					var currentAnswer = remainingProblems[ identifier ];
-					remainingProblems[ identifier ] = new Array();
-					remainingProblems[ identifier ].push( currentAnswer );
-					remainingProblems[ identifier ].push( newAnswer );
+					var currentAnswer = problemsRemaining[ identifier ];
+					problemsRemaining[ identifier ] = new Array();
+					problemsRemaining[ identifier ].push( currentAnswer );
+					problemsRemaining[ identifier ].push( newAnswer );
 				}
 			},
+			
 			'nextProblem' : function() {
-				if ( remainingProblems.length === 0 ) { return null; }
-				var nextProblemIndex = Math.floor( Math.random() * remainingProblems.length );
-				var nextProblem = problems[ remainingProblems[ nextProblemIndex ] ];
-				nextProblem[ 'identifier' ] = remainingProblems[ nextProblemIndex ];
-				remainingProblems.splice( nextProblemIndex, 1 );
-				return nextProblem;
+				
 			},
+			
+			'randomProblemRemaining' : function() {
+				
+			},
+			
+			'randomProblemIncorrect' : function() {
+				
+			},
+			
+			'randomProblemCorrect' : function() {
+				
+			},
+			
+			'currentProblem' : function() {
+				if ( currentProblem === undefined ) { nextProblemLookupByType[ problemType ](); }
+				if ( currentProblem === null ) { return null; }
+				nextProblemLookupByType[ problemType ]();
+				var identifiedCurrentProblem = problems[ currentProblem ];
+				console.log(problems[currentProblem], currentProblem)
+				identifiedCurrentProblem[ 'identifier' ] = currentProblem;
+				return identifiedCurrentProblem;
+			},
+			
 			'checkAnswer' : function( identifier, answer ) {
-				var currentProblem = problems[ identifier ];
-				if ( currentProblem.answerOrAnswers() instanceof Array ) {
-					var problemCorrect = false;
-					currentProblem.answerOrAnswers().forEach(
-						function( currentProblemAnswer ) {
-							if ( currentProblemAnswer === answer ) {
+				var problem = problems[ identifier ];
+				var problemCorrect = false;
+				if ( problem.answerOrAnswers() instanceof Array ) {
+					problem.answerOrAnswers().forEach(
+						function( problemAnswer ) {
+							if ( problemAnswer === answer ) {
 								problemCorrect = true;
 							}
 						}
 					);
-					return problemCorrect;
 				} else {
-					return currentProblem.answerOrAnswers() === answer;
+					problemCorrect = problem.answerOrAnswers() === answer;
 				}
-				return false;
+				if ( problemCorrect ) {
+					problemSets[ 'correct' ].push( identifier );
+				} else {
+					problemSets[ 'incorrect' ].push( identifier );
+					for ( var i = 0; i < retries; i++ ) {
+						problemSets[ 'retries' ].push( identifier );
+					}
+				}
+				return problemCorrect;
 			},
+			
 			'setTimesToRetryProblems' : function( newRetries ) {
-				retries = newRetries;
+				if ( newRetries === Math.floor( newRetries ) && newRetries >= 0 ) {
+					retries = newRetries;
+				} else {
+					throw 'The number of retries must be an integer greater than or equal to zero.';
+				}
+			},
+			
+			'setProblemType' : function( newProblemType ) {
+				if ( ! nextProblemLookupByType[ newProblemType ] ) {
+					throw 'The problem type "' + newProblemType + '" is not one of the following: "random", "remaining", "incorrect", or "correct".';
+				}
+				problemType = newProblemType;
 			}
 		};
 	};
@@ -82,9 +229,11 @@ var Tutorial = function() {
 			'prompt' : function() {
 				return prompt;
 			},
+			
 			'answerOrAnswers' : function() {
 				return answerOrAnswers;
 			},
+			
 			'explanation' : function() {
 				return explanation;
 			}
@@ -97,6 +246,7 @@ var Tutorial = function() {
 			sections[ identifier ] = newSection;
 			return newSection;
 		},
+		
 		getSection : function( identifier ) {
 			return sections[ identifier ];
 		}
